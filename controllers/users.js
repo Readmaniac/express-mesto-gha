7b82d3d84872036b6764a5b470b80882397d40ef
Stanislav Simakov <stanislav.sima@yandex.ru>
2023-03-20 20:06:14 +0300

    login create and login controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,62 @@
-const User = require('../models/user');
-const { ERROR_BAD_REQUEST, ERROR_NOT_FOUND, ERROR_INTERNAL_SERVER } = require('../errors/errors');
+const bcrypt = require('bcrypt');
+const jsonwebtoken = require('jsonwebtoken');
+const { JWT_SECRET } = require('../utils/constants');
 
-function createUser(req, res) {
-  const { name, about, avatar } = req.body;
+const User = require('../models/user');
+const {
+  ERROR_BAD_REQUEST,
+  ERROR_NOT_FOUND,
+  ERROR_INTERNAL_SERVER,
+  CONFLICT_REQUEST,
+  UNAUTHORIZED_ERROR,
+} = require('../errors/errors');
+
+function createUser(req, res, next) {
+  const {
+    name,
+    about,
+    avatar,
+    email,
+    password,
+  } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.code === 11000) {
+        res.status(CONFLICT_REQUEST).send({ message: 'Пользователь с такими данными уже существует' });
+      } else if (err.name === 'ValidationError') {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при создании пользователя' });
+      } else {
+        next(err);
+      }
+    });
+}
+
+function loginUser(req, res, next) {
+  const { email, password } = req.body;
 
   User
-    .create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => (
-      err.name === 'ValidationError'
-        ? res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при создании пользователя' })
-        : res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка на сервере' })
-    ));
+    .findUser(email, password)
+    .orFail(() => res.status(UNAUTHORIZED_ERROR).send({ message: 'Пользователь не найден' }))
+    .then((user) => bcrypt.compare(password, user.password).then((matched) => {
+      if (matched) {
+        return user;
+      }
+      return res.status(UNAUTHORIZED_ERROR).send({ message: 'Пользователь не найден' });
+    }))
+    .then((user) => {
+      const jwt = jsonwebtoken.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+      res.send({ _id: jwt });
+    })
+    .catch(next);
 }
 
 function getUsersInfo(req, res) {
@@ -23,12 +68,10 @@ function getUsersInfo(req, res) {
 
 function getUserInfo(req, res) {
   const { id } = req.params;
-
   User
     .findById(id)
     .then((user) => {
       if (user) return res.send({ data: user });
-
       return res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь по указанному id не найден' });
     })
     .catch((err) => (
--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      default: 'Жак-Ив Кусто',
       validate: {
         validator: ({ length }) => length >= 2 && length <= 30,
         message: 'Имя пользователя должно быть длиной от 2 до 30 символов',
@@ -13,7 +14,7 @@ const userSchema = new mongoose.Schema(
 
     about: {
       type: String,
-      required: true,
+      default: 'Исследователь',
       validate: {
         validator: ({ length }) => length >= 2 && length <= 30,
         message: 'Информация о пользователе должна быть длиной от 2 до 30 символов',
@@ -22,7 +23,31 @@ const userSchema = new mongoose.Schema(
 
     avatar: {
       type: String,
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+      validate: {
+        validator(url) {
+          return validator.isURL(url);
+        },
+        message: 'Нужно ввести корректный URL',
+      },
+    },
+
+    email: {
+      type: String,
       required: true,
+      unique: true,
+      validate: {
+        validator(email) {
+          return validator.isEmail(email);
+        },
+        message: 'Поле "email" должно быть валидным email-адресом',
+      },
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false,
     },
   },
   {
